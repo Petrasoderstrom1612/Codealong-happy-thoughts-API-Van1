65e69ea755c075ec480c61c255afaa874827bded
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2022-12-12 12:01:34 +0100

    updated repository on Github

--- server.js
+++ server.js
@@ -6,9 +6,26 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+const Person = mongoose.model('Person', {
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 500
+  },
+  height: {
+    type: Number,
+    required: true,
+    min: 5
+  },
+  birthdate: {
+   type: Date,
+   default: Date.now
+  }
+});
+
+new Person({name: "Van", height: 150}).save;
+
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -20,6 +37,28 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+// The post request
+app.post('/people', async (req, res) =>{
+  //PROMISES
+  new Person(req.body).save()
+  .then((person) => {
+    res.status(200).json(person)
+  })
+  .catch((err) => {
+    res.status(400).json({message: 'Could not save person', errors: err.errors});
+  })
+})
+
+  //TRY CATCH FORM
+//   try{
+//   const person = await new Person(req.body).save();
+//   res.status(200).json(person);
+//   }catch(err){
+//     res.status(400).json({message: 'Could not save person', errors: err.errors});
+//   }
+// });
+
 
 // Start the server
 app.listen(port, () => {
